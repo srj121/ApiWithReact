aac5a119dc7a6bc10930d929eb209489f27c23c8
Suraj <surajbade39.sb@gmail.com>
2023-03-27 19:49:54 +0530

    .

--- src/main/java/com/main/ApiWithReact/controller/Controller.java
+++ src/main/java/com/main/ApiWithReact/controller/Controller.java
@@ -5,6 +5,8 @@ import com.main.ApiWithReact.entity.Employees;
 import com.main.ApiWithReact.service.ServiceLayer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,23 +15,19 @@ import java.util.List;
 
 
 @RestController
-@CrossOrigin(value = {"http://localhost:3000"})
+//@CrossOrigin(value = {"http://localhost:3000"})
+@CrossOrigin
 public class Controller {
     private static final Logger logger = LoggerFactory.getLogger(Controller.class);
-    private static final String page = "list-employees";
     private final ServiceLayer serviceLayer;
      Controller(ServiceLayer serviceLayer)
      {this.serviceLayer = serviceLayer;}
 
-
     //..................................................................................................................
-
-
 
     @GetMapping("/data")
     public List<Employees> getData() {
          return serviceLayer.getEmpService();
-
     }
 
     //..................................................................................................................
@@ -42,31 +40,32 @@ public class Controller {
 
     }
     //..................................................................................................................
-    @GetMapping("/deleteEmpById")
-    public ModelAndView  deleteById( @RequestParam String id){
+    @DeleteMapping("/deleteEmpById/{id}")
+    public ResponseEntity<Employees>  deleteById( @PathVariable String id){
         logger.info(id);
-        ModelAndView mav = new ModelAndView(page);                      //** Delete By Id
-        mav.addObject("emp", serviceLayer.deleteById(id));
-        return mav;
+        serviceLayer.deleteById(id);
+        return new  ResponseEntity<>(HttpStatus.OK);
     }
     //..................................................................................................................
 
     @GetMapping("/getEmpById")
     public ModelAndView getEmpById(@RequestParam String id){
         logger.info(id);
-        ModelAndView mav = new ModelAndView(page);                      //**Get By ID API
+        ModelAndView mav = new ModelAndView();                      //**Get By ID API
         mav.addObject("emp",serviceLayer.getById(id));
         return mav;
     }
     //..................................................................................................................
-    @GetMapping("/addEmp")
-    public ModelAndView addEmp(@RequestParam String id,@RequestParam String name, @RequestParam String address){
-        ModelAndView mav = new ModelAndView(page);
-        Employees emp = new Employees(id,name,address);
+    @PostMapping("/addEmp/{id}/{name}/{address}")
+    public ResponseEntity<Employees> addEmp(@PathVariable String id, @PathVariable String name, @PathVariable String address){
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println(address);
+        Employees emp = new Employees(id, name, address);
         logger.info("{}",emp);                                              //**Post API
-        mav.addObject("emp",emp);
         serviceLayer.saveEmp(emp);
-        return mav;
+        return new ResponseEntity<>(emp, HttpStatus.CREATED);
+
     }
 //......................................................................................................................
 
